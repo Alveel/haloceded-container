0fd5fe54d06b908869145060bfa4377d15ae96c8
Alwyn <alwyn@kik.pw>
2025-05-08 15:20:27 +0200

    Run cmd in wine by calling it with cmd.exe

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "haloceded" ]; then
   # Run Halo server with wine in a virtual frame buffer
-   xvfb-run wine cmd /c haloceded.exe -exec data/init.txt
+   xvfb-run --auto-servernum wine cmd.exe /c haloceded.exe -exec data/init.txt
 else
   # If the first argument is not "haloceded", try to execute the command
   exec "$@"
